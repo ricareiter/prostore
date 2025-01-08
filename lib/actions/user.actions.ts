f2581e36801ae4143ce03910eddd3d58df2200f6
Ricardo Reiter <ricardoreiterr@gmail.com>
2025-01-08 20:13:12 -0300

    update error handling

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
+import { formatError } from "../utils";
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -63,6 +64,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
       throw error;
     }
 
-    return { success: false, message: "User was not registered" };
+    return { success: false, message: formatError(error) };
   }
 }
